9f1ff3c1e402e941a8b9027638e2c8b6171bbb66
Shashank-2004 <misrashashank06@gmail.com>
2026-03-21 20:23:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+
+const App = () => {
+
+  const [counter, setCounter] = useState(() => {
+    const saved = localStorage.getItem("counter");
+    return saved !== null ? JSON.parse(saved) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("counter", JSON.stringify(counter));
+  }, [counter]);
+
+  //let counter=0
+  const addValue=()=>{
+    setCounter(counter+1)
+  }
+
+  const removeValue=()=>{
+    setCounter(0)
+  }
+
+  return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+
+      <div className="bg-sky-400 text-center p-6 rounded-3xl w-[90%] max-w-sm">
+        <h1 className='mb-4 pt-4 text-3xl text-white'>COUNTER</h1>
+        <img src='https://i.pinimg.com/736x/6b/24/fc/6b24fc9012ca3b6debc5892a33335d9b.jpg' className='h-50 ml-22.5 w-35'/>
+        <h2 className='text-2xl mt-2 mb-4 text-white'>Value : {counter}</h2>
+        <button onClick={addValue} className='bg-black text-white w-35 p-1 rounded-2xl text-xl'>Count</button>
+        <br/>
+        <button onClick={removeValue} className='bg-red-500 text-white w-35 p-1 mt-2 rounded-2xl text-xl'>Reset</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
