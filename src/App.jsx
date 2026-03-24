1ffc2320c65e4c8abf7038f9890015c67fbe16c4
Shashank-2004 <misrashashank06@gmail.com>
2026-03-24 19:21:56 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ const App = () => {
   }
 
   return (
-      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 animate-gradient bg-[length:200%_200%]">
 
-      <div className="bg-sky-400 text-center p-6 rounded-3xl w-[90%] max-w-sm">
+      <div className="bg-white/20 backdrop-blur-lg border border-white/30 text-center p-6 rounded-3xl w-[90%] max-w-sm shadow-xl">
         <h1 className='mb-4 pt-4 text-3xl text-white'>COUNTER</h1>
-        <img src='https://i.pinimg.com/736x/6b/24/fc/6b24fc9012ca3b6debc5892a33335d9b.jpg' className='h-50 ml-22.5 w-35'/>
+        <img src='https://i.pinimg.com/736x/6b/24/fc/6b24fc9012ca3b6debc5892a33335d9b.jpg' className='h-50 ml-24.5 w-35'/>
         <h2 className='text-2xl mt-2 mb-4 text-white'>Value : {counter}</h2>
         <button onClick={addValue} className='bg-black text-white w-35 p-1 rounded-2xl text-xl'>Count</button>
         <br/>
