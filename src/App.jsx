eb0d65a922dcb018e7daaa7cb67a773fb87c78e3
Shashank-2004 <misrashashank06@gmail.com>
2026-04-10 00:27:28 +0530

    don't change

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,8 @@ const App = () => {
       <div className="bg-white/20 backdrop-blur-lg border border-white/30 text-center p-6 rounded-3xl w-[90%] max-w-sm shadow-xl">
       <div className='flex mb-4 justify-center'>
         <h1 className='pt-4 text-3xl ml-20 text-white'>COUNTER</h1>
-        <button onClick={removeValue} className='bg-red-500 text-white p-1 ml-10 w-10 h-10 rounded-full text-xl'>R</button>
+        <button onClick={removeValue} className='bg-red-500 text-white p-1 ml-10 w-10 h-10 rounded-full text-xl transition duration-150 ease-in-out 
+             active:scale-95 active:bg-red-700'>R</button>
       </div>
         <img src='https://i.pinimg.com/736x/6b/24/fc/6b24fc9012ca3b6debc5892a33335d9b.jpg' className='w-40 sm:w-48 md:w-56 mx-auto rounded-lg'/>
         <h2 className='text-2xl mt-2 mb-4 text-white'>Value : {counter}</h2>
