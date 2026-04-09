d801e35dae535f3894559e35f352311654a275ec
Shashank-2004 <misrashashank06@gmail.com>
2026-04-10 00:20:45 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,13 @@ const App = () => {
       <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 animate-gradient bg-[length:200%_200%]">
 
       <div className="bg-white/20 backdrop-blur-lg border border-white/30 text-center p-6 rounded-3xl w-[90%] max-w-sm shadow-xl">
-        <h1 className='mb-4 pt-4 text-3xl text-white'>COUNTER</h1>
-        <img src='https://i.pinimg.com/736x/6b/24/fc/6b24fc9012ca3b6debc5892a33335d9b.jpg' className='h-50 ml-24.5 w-35'/>
+      <div className='flex mb-4 justify-center'>
+        <h1 className='pt-4 text-3xl ml-20 text-white'>COUNTER</h1>
+        <button onClick={removeValue} className='bg-red-500 text-white p-1 ml-10 w-10 h-10 rounded-full text-xl'>R</button>
+      </div>
+        <img src='https://i.pinimg.com/736x/6b/24/fc/6b24fc9012ca3b6debc5892a33335d9b.jpg' className='w-40 sm:w-48 md:w-56 mx-auto rounded-lg'/>
         <h2 className='text-2xl mt-2 mb-4 text-white'>Value : {counter}</h2>
-        <button onClick={removeValue} className='bg-red-500 text-white w-35 p-1 rounded-2xl text-xl'>RESET</button>
-        <br/>
-        <button onClick={addValue} className='bg-purple-500 text-white w-35 p-1 mt-2 rounded-2xl text-xl'>COUNT</button>
+        <button onClick={addValue} className='bg-purple-500 text-white w-35 p-1 rounded-2xl text-xl'>COUNT</button>
       </div>
     </div>
   )
