ae1b0ab3a59df3499d236ccedfd0c71580ca85b2
Shashank-2004 <misrashashank06@gmail.com>
2026-04-14 23:41:20 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,9 @@ const App = () => {
   }
 
   return (
-      <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 animate-gradient bg-[length:200%_200%]">
+      <div className="flex justify-center items-center min-h-screen">
 
-      <div className="bg-white/20 backdrop-blur-lg border border-white/30 text-center p-6 rounded-3xl w-[90%] max-w-sm shadow-xl">
+      <div className="bg-sky-400 backdrop-blur-lg border border-white/30 text-center p-6 rounded-3xl w-[90%] max-w-sm shadow-xl">
       <div className='flex mb-4 justify-center'>
         <h1 className='pt-4 text-3xl ml-20 text-white'>COUNTER</h1>
         <button onClick={removeValue} className='bg-red-500 text-white p-1 ml-10 w-10 h-10 rounded-full text-xl transition duration-150 ease-in-out 
@@ -34,8 +34,8 @@ const App = () => {
         <h2 className='text-2xl mt-2 mb-4 text-white'>Value : {counter}</h2>
         <button 
             onClick={addValue} 
-            className="bg-purple-500 text-white w-full py-2 rounded-2xl text-xl 
-             transition duration-150 ease-in-out 
+            className="bg-purple-500 text-white w-40 py-2 rounded-2xl text-xl 
+             transition duration-50 ease-in-out 
              active:scale-95 active:bg-purple-700"
             >
   COUNT
