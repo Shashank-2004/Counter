6a51e71627987e6785c34ea20da9cf73b2bbdab8
Shashank-2004 <misrashashank06@gmail.com>
2026-06-22 21:54:35 +0530

    Fix Vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/Counter/',  // 👈 must match your repo name
+  plugins: [react(), tailwindcss()],
 })
